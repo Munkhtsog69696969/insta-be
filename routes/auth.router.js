a48678da4326ea2b8247b2131928a99af61f2d21
heisenberg <sda@example.com>
2023-05-30 17:19:50 +0800

    s

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -6,10 +6,10 @@ const authRouter = express.Router();
 
 authRouter
     .get("/google" , passport.authenticate("google" , {scope:["profile"]}))
-    .get("/google/callback" , passport.authenticate("google" , {
-        successRedirect:"http://localhost:3000",
-        failureRedirect:"https://insta-be-vm35.onrender.com/auth/login/failure"
-    }))
+    // .get("/google/callback" , passport.authenticate("google" , {
+    //     successRedirect:"http://localhost:3000",
+    //     failureRedirect:"https://insta-be-vm35.onrender.com/auth/login/failure"
+    // }))
     .get("/login/success",(req,res)=>{
         if(req.user){
             res.status(200).json({ 
